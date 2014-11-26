d2b91560729a9a07668ffa4f40e43d255c284f45
Jeffrey Bao-shen LEE <eastarstormlee@gmail.com>
2014-11-27 00:59:54 +0800

    Add some examples.

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,15 +1,25 @@
 public class ArrayTest {
-	private static int[][] array={
-			{1,2,5,9,10},
-			{}
-	};
-	
-	public static void main (String... args){
+	private static int[][] array = { {}, { 1, 2, 5, 9, 10 }, {} };
+
+	public static void main(String... args) {
 		System.out.println(array);
-		
-		int[][][] myArr1=new int[8][][];
-		int[][][] myArr2=new int[1][9][];
+
+		int[][][] myArr1 = new int[8][][];
+		int[][][] myArr2 = new int[1][9][10];
 		System.out.println(myArr1.getClass());
 		System.out.println(myArr2.getClass());
+	}
+
+	public void foo() {
+		Object[][] myArr = new Object[10][12];
+		myArr[5][6] = new Object();
+
+		Object o;
+		try {
+			o = new Object();
+			System.out.println(o);
+		} finally {
+			o = null;
+		}
 	}
 }
---
+++ src/ConvertTest.java
@@ -0,0 +1,8 @@
+
+
+public class ConvertTest {
+	public static void main(String[] args) {
+		Object o= new Integer(1);
+		System.out.println(o);
+	}
+}
