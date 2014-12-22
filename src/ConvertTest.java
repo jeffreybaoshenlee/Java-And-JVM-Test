81b2469b76f1907f4b6bbaf2cf3ba918357d5580
Jeffrey Bao-shen LEE <eastarstormlee@gmail.com>
2014-12-22 17:00:46 +0800

    Add some examples.

--- src/ConvertTest.java
+++ src/ConvertTest.java
@@ -1,8 +1,9 @@
-
-
 public class ConvertTest {
 	public static void main(String[] args) {
-		Object o= new Integer(1);
+		Object o = new Integer(1);
 		System.out.println(o);
+
+		Object obj = null;
+		obj = (Integer) obj;
 	}
 }
