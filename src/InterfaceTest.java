91bc47599606469482c5f176074bcf45d9e4e3c7
Jeffrey Bao-shen LEE <eastarstormlee@gmail.com>
2015-01-09 17:16:43 +0800

    Add some examples.

--- src/InterfaceTest.java
+++ src/InterfaceTest.java
@@ -1,12 +1,17 @@
+import java.util.Random;
+
 public class InterfaceTest implements MySubInterface {
 	private static int myInt;
-	
-	private interface MyPrivateInterface{
+
+	private interface MyPrivateInterface {
+		public static int value = new Random().nextInt();
+
 		default void foo() {
+			System.out.println(value);
 			System.out.println(myInt);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		InterfaceTest interfaceTest = new InterfaceTest();
 		Class<?> clazz = interfaceTest.getClass();
@@ -14,7 +19,7 @@ public class InterfaceTest implements MySubInterface {
 		Class<?> firstInterface = interfaces[0];
 		System.out.println(firstInterface);
 		System.out.println(firstInterface.getSuperclass());
-		
+
 		interfaceTest.foo();
 		MyPrivateInterface myPrivateInterface = new MyPrivateInterface() {
 		};
